15de6d569773ccdc47f7d58a6135f86db57664d7
Ziad Arafat <ziada@nmsu.edu>
2024-03-18 14:36:59 -0600

    Built structure to concurrently look for detections and post detections as they come in.

---
+++ main.py
@@ -0,0 +1,43 @@
+import aiohttp
+import asyncio
+import time
+import random
+from uuid import uuid4
+from collections import deque
+
+detections_queue = deque(maxlen=100)
+
+async def post_detection_random(session, data):
+        async with session.post('http://localhost:3000/new_detection', json=data) as response:
+                print(await response.text())
+                
+async def post_detections_from_queue():
+        session = aiohttp.ClientSession()
+        while True:
+                if len(detections_queue) > 0:
+                        data = detections_queue.popleft()
+                        await post_detection_random(session, data)
+                print("Next post in 2 seconds")
+                await asyncio.sleep(2)
+       
+
+async def main():
+        while True:
+                detection_roll = random.randint(0, 100)
+                if detection_roll > 50:
+                        detections_queue.appendleft({
+                                "id": str(uuid4()),
+                                "time": time.time(),
+                                "images": ["image1.jpg", "image2.jpg"],
+                                "location": [random.randint(0, 100), random.randint(0, 100)]
+                        })
+                print("Next detection in 1 second")
+                await asyncio.sleep(1)
+
+# Run the main function in an event loop
+loop = asyncio.get_event_loop()
+loop.create_task(main())
+loop.create_task(post_detections_from_queue())
+loop.run_forever()
+
+                        
