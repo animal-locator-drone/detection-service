129cf00c2940e3cd9367ad2062728f639e3bc4b8
MatthewMMontoya <montoyam.matthew@gmail.com>
2024-03-22 17:17:55 -0600

    trained model1

--- yolo.py
+++ yolo.py
@@ -1,4 +1,20 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")  # initialize model
-results = model(source=0, show=True, conf=0.4, save=False)
+
+def main():
+
+    model = YOLO("yolov8n.pt")  # initialize model
+    # results = model(source=0, show=True, conf=0.4, save=False) # run model on webcam stream
+    results = model.train(
+        data="/Volumes/X9 Pro/CS/SeniorProj/Data/wolf.v11i.yolov8/data.yaml",  # must modify to appropriate path # ideally everyone has their custom yaml in their pwd
+        epochs=3,
+        device="mps",  # mac m-series(apple silicon)ONLY optimization parameter
+    )
+
+
+def train_model():
+    pass
+
+
+if __name__ == "__main__":
+    main()
