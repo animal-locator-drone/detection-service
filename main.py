878a2aa53a0b085f9b2b116766fb80251c3ef7df
Ziad Arafat <ziada@nmsu.edu>
2024-04-01 15:19:39 -0600

    integrate new detector and use multiprocessing instead

--- main.py
+++ main.py
@@ -1,48 +1,50 @@
-import aiohttp
-import asyncio
+import requests
 import time
 import random
 from uuid import uuid4
 from collections import deque
+from multiprocessing import Process, Queue
 
-detections_queue = deque(maxlen=100)
+from dog_detector import *
 
-async def post_detection_random(session, data):
-        async with session.post('http://localhost:3000/new_detection', json=data) as response:
-                print(await response.text())
-                
-async def post_detections_from_queue():
-        session = aiohttp.ClientSession()
-        while True:
-                if len(detections_queue) > 0:
-                        data = detections_queue.popleft()
-                        await post_detection_random(session, data)
-                print("Next post in 2 seconds")
-                await asyncio.sleep(2)
-       
 
-async def main():
-        while True:
-                detection_roll = random.randint(0, 100)
-                if detection_roll > 50:
-                        # Somewhere we will need to also asyncronously save the images to disk
-                        detections_queue.append({
-                                "id": str(uuid4()),
-                                "time": time.time(),
-                                "images": [
-                                        "https://www.akc.org/wp-content/uploads/2017/11/Afghan-Hound-at-the-National-Championship.jpg",
-                                        "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/02151216/Afghan-Hound-standing-in-a-garden.jpg",
-                                        "https://i.ytimg.com/vi/BbAjcUdb_io/maxresdefault.jpg"
-                                ],
-                                "location": [random.randint(0, 100), random.randint(0, 100)]
-                        })
-                print("Next detection in 1 second")
-                await asyncio.sleep(1)
+def post_detection_random(data):
+    response = requests.post('http://localhost:3000/new_detection', json=data)
+    print(response)
 
-# Run the main function in an event loop
-loop = asyncio.get_event_loop()
-loop.create_task(main())
-loop.create_task(post_detections_from_queue())
-loop.run_forever()
 
-                        
+def post_detections_from_queue(queue):
+#     time.sleep(5)
+    while True:
+        if not queue.empty():
+            print("posting detection")
+            data = queue.get()
+            post_detection_random(data)
+
+
+def main(queue):
+#     time.sleep(5)
+    for confidences, boxes, cropped_images in generate_predictions():
+        data = {
+            "id": str(uuid4()),
+            "time": time.time(),
+            "images": cropped_images,
+            "location": [random.randint(0, 100), random.randint(0, 100)]
+        }
+        print("appending to queue")
+        queue.put(data)
+
+
+if __name__ == '__main__':
+    detections_queue = Queue(maxsize=100)
+
+    main_process = Process(target=main, args=(detections_queue,))
+    main_process.start()
+    print("main process started")
+
+    post_process = Process(target=post_detections_from_queue, args=(detections_queue,))
+    post_process.start()
+    print("post process started")
+
+    main_process.join()
+    post_process.join()
